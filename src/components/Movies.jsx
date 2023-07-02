4cc8d2bb06c04f6fdcbd80a3ed6a8ee8a78fb24c
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-07-02 20:41:49 +0100

    Handle deleting a movie

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,8 +2,11 @@ import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 class Movies extends Component {
   state = { movies: getMovies() };
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
   render() {
-    console.log(this.state.movies);
     return (
       <table className="table">
         <thead>
@@ -23,7 +26,12 @@ class Movies extends Component {
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
               <td>
-                <button className="btn btn-sm btn-danger">Delete</button>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-sm btn-danger"
+                >
+                  Delete
+                </button>
               </td>
             </tr>
           ))}
