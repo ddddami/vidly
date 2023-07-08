1d4947375895adb75cd687673e3a7fb2381f6781
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-07-09 00:16:07 +0100

    Add validation on change

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -18,6 +18,15 @@ class LoginForm extends Component {
       errors.password = "Password is required.";
     return Object.keys(errors).length === 0 ? null : errors;
   };
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username field is required";
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password field is required";
+    }
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
     const errors = this.validate();
@@ -28,9 +37,14 @@ class LoginForm extends Component {
     console.log("Submitted");
   };
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
     const { account, errors } = this.state;
