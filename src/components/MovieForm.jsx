a99f14d79be7255c939531aa7f5810e639cd1baa
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-07-08 01:49:10 +0100

    Link movie titles to movie form

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -1,5 +1,16 @@
+import { useNavigate, useParams } from "react-router";
+
 const MovieForm = () => {
-  return <p>Movie Form</p>;
+  const navigate = useNavigate();
+  const { id } = useParams();
+  return (
+    <div>
+      <p>Movie Form {id}</p>
+      <button className="btn btn-primary" onClick={() => navigate("/")}>
+        Save
+      </button>
+    </div>
+  );
 };
 
 export default MovieForm;
--- src/components/MoviesTable.jsx
+++ src/components/MoviesTable.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Like from "./common/Like";
 import Table from "./common/Table";
 
 class MoviesTable extends Component {
   columns = [
-    { path: "title", label: "Title" },
+    {
+      path: "title",
+      label: "Title",
+      content: (movie) => (
+        <Link style={{ textDecoration: "none" }} to={`/movies/${movie._id}`}>
+          {movie.title}
+        </Link>
+      ),
+    },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
