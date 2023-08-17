ad02ff0b511c6a2709405a43a766294ef5bfc85e
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-08-17 10:09:30 +0100

    Refactor: Cleanup movies url

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,19 +1,23 @@
 import http from "./httpService";
 import config from "../config.json";
+const moviesUrl = config.apiUrl + "/movies";
+function movieUrl(id) {
+  return `${config.apiUrl}/movies/${id}/`;
+}
 export function getMovies() {
-  return http.get(config.apiUrl + "/movies");
+  return http.get(moviesUrl);
 }
 export function getMovie(id) {
-  return http.get(config.apiUrl + "/movies/" + id);
+  return http.get(movieUrl(id));
 }
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(config.apiUrl + "/movies/" + movie._id + "/", body);
+    return http.put(movieUrl(movie._id), body);
   }
-  return http.post(config.apiUrl + "/movies/", movie);
+  return http.post(moviesUrl, movie);
 }
 export function deleteMovie(id) {
-  return http.delete(config.apiUrl + "/movies/" + id);
+  return http.delete(movieUrl(id));
 }
