43ac9bca69db503593f3a4d21ebe7b525a792cbf
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-08-18 20:25:23 +0100

    Fix: expected errors not passed to catch block

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -8,8 +8,8 @@ axios.interceptors.response.use(null, (error) => {
     error.response.status < 500;
   if (!expectedError) {
     logger.log("error");
-    return Promise.reject(error);
   }
+  return Promise.reject(error);
 });
 
 export default {
