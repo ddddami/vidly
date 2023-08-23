838d21b58cf4c4001f2457f3c9f925afc1be3497
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-08-23 22:47:26 +0100

    Protect routes based on user permissions

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -1,13 +1,27 @@
 import React, { useContext } from "react";
-import UserContext from "../context/UserContext";
 import { Navigate, Outlet } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import UserContext from "../context/UserContext";
 
 const PrivateRoutes = () => {
   const { user } = useContext(UserContext);
-  if (!user) {
-    return <Navigate to="/login" />;
+  if (user?.isAdmin) {
+    return <Outlet />;
   }
-  return <Outlet />;
+  toast(
+    (t) => (
+      <p>
+        Heyy, you're not logged in as an admin. <br /> Login with{" "}
+        <span className="text-primary">realdami</span> (username) and{" "}
+        <span className="text-primary">realpassword</span> (password) to get
+        admin access.
+      </p>
+    ),
+    {
+      duration: 6000,
+    }
+  );
+  return <Navigate to="/login" />;
 };
 
 export default PrivateRoutes;
